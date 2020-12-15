0843c35c00ebb6c3f4ba73d44edd25f7702b6762
RavaszTamas <ravaszt88@gmail.com>
2020-12-15 16:11:17 +0200

    1st iteration for send and recv

--- src/ro/ubb/Main.java
+++ src/ro/ubb/Main.java
@@ -1,8 +1,10 @@
 package ro.ubb;
 
 import mpi.MPI;
+import ro.ubb.models.Board;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.stream.Collectors;
@@ -10,11 +12,11 @@ import java.util.stream.IntStream;
 
 public class Main {
 
-  final static int MAIN_PROCESS = 0;
+  static final int MAIN_PROCESS = 0;
   public static final int SIZE = 4;
 
+  static final List<Integer> directions = new ArrayList<>();
 
-  final static List<Integer>  directions=new ArrayList<>();
   static {
     directions.add(-1);
     directions.add(1);
@@ -22,6 +24,12 @@ public class Main {
     directions.add(SIZE);
   }
 
+  public static final List<Integer> goalBoard;
+
+  static {
+    goalBoard = IntStream.range(1, Main.SIZE * Main.SIZE).boxed().collect(Collectors.toList());
+    goalBoard.add(0);
+  }
 
   public static void main(String[] args) {
     MPI.Init(args);
@@ -30,20 +38,31 @@ public class Main {
 
     List<Integer> initialBoard = new ArrayList<>();
 
-    initialBoard.add(4 );initialBoard.add(8 );initialBoard.add(6 );initialBoard.add(10 );
+    initialBoard.add(4);
+    initialBoard.add(8);
+    initialBoard.add(6);
+    initialBoard.add(10);
 
-    initialBoard.add(2 );initialBoard.add(9 );initialBoard.add(5 );initialBoard.add(14 );
+    initialBoard.add(2);
+    initialBoard.add(9);
+    initialBoard.add(5);
+    initialBoard.add(14);
 
-    initialBoard.add(0 );initialBoard.add(1 );initialBoard.add(3 );initialBoard.add(11 );
+    initialBoard.add(0);
+    initialBoard.add(1);
+    initialBoard.add(3);
+    initialBoard.add(11);
 
-    initialBoard.add(13 );initialBoard.add(15 );initialBoard.add(7 );initialBoard.add(12 );
+    initialBoard.add(13);
+    initialBoard.add(15);
+    initialBoard.add(7);
+    initialBoard.add(12);
 
-    List<Integer> goalBoard = IntStream.range(1, Main.SIZE * Main.SIZE).boxed().collect(Collectors.toList());
+//    System.out.println("Size " + size + " me " + me);
 
-    if(me == MAIN_PROCESS) {
-      puzzleSolver(initialBoard,size);
-    }
-    else{
+    if (me == MAIN_PROCESS) {
+      puzzleSolver(initialBoard, size);
+    } else {
       puzzleSolverWorker();
     }
 
@@ -52,33 +71,71 @@ public class Main {
 
   private static void puzzleSolver(List<Integer> initialBoard, int totalNumberOfProcesses) {
     int numberOfWorkers = totalNumberOfProcesses - 1;
-    int expanded = 0;
-    PriorityQueue<List<Integer>> priorityQueue = new PriorityQueue<>();
-    while(true) {
-        if (expanded % 10000 == 0) {
-            if (handleTick()) {
-                return;
-            }
-        }
-        expanded++;
-        boolean notFinished=true;
-        while(notFinished) {
-            //TODO MPI RECEIVE
-            if(notFinished==true){
-                notFinished=false;
-            }
-            priorityQueue.add()
-        }
+    List<Integer> initialPath = new ArrayList<>();
+    PriorityQueue<Board> priorityQueueOfBoards = new PriorityQueue<>();
+    priorityQueueOfBoards.add(new Board(initialBoard, initialPath, 0));
 
+//    System.out.println(priorityQueueOfBoards.peek());
+    int[] sendBuffer;
+
+    List<Integer> currentPath = new ArrayList<>();
+
+    Board theGoalBoard = new Board(goalBoard,new ArrayList<>(),0);
+
+    while (true) {
+      int itemsSent = 0;
+      while (itemsSent < numberOfWorkers && priorityQueueOfBoards.size() > 0) {
+        Board current = priorityQueueOfBoards.poll();
+        itemsSent += 1;
+
+        sendBuffer = current.flattenBoard();
+        int[] sizeToSend = {sendBuffer.length};
+
+//        System.out.println("Sent to " + itemsSent + ": " + Arrays.toString(sendBuffer));
+
+        MPI.COMM_WORLD.Send(sizeToSend, 0, 1, MPI.INT, itemsSent, 2);
+        MPI.COMM_WORLD.Send(sendBuffer, 0, sizeToSend[0], MPI.INT, itemsSent, 1);
+      }
+
+      for (int i = 1; i <= itemsSent; i++) {
+        int[] sizeToRecieve = {0};
+        int[] numberOfItemsToRead = {0};
+
+        MPI.COMM_WORLD.Recv(numberOfItemsToRead, 0, 1, MPI.INT, itemsSent, 3);
+        for (int j = 0; j < numberOfItemsToRead[0]; j++) {
+          MPI.COMM_WORLD.Recv(sizeToRecieve, 0, 1, MPI.INT, itemsSent, 2);
+          int[] result = new int[sizeToRecieve[0]];
+          MPI.COMM_WORLD.Recv(result, 0, sizeToRecieve[0], MPI.INT, itemsSent, 1);
+          Board newBoard = Board.fromArray(result);
+
+          priorityQueueOfBoards.add(newBoard);
+          if (newBoard.equals(theGoalBoard)) {
+            System.out.println("Hurray for soltuion");
+          }
+        }
+      }
     }
-
   }
 
-    private static boolean handleTick() {
-      return false;
+  private static void puzzleSolverWorker() {
+
+    while (true) {
+      int[] sizeToRecieve = {0};
+      int[] sizeToSend = {0};
+
+      MPI.COMM_WORLD.Recv(sizeToRecieve, 0, 1, MPI.INT, 0, 2);
+
+      int[] recievingBuffer = new int[sizeToRecieve[0]];
+
+      MPI.COMM_WORLD.Recv(recievingBuffer, 0, sizeToRecieve[0], MPI.INT, 0, 1);
+
+//      System.out.println("Sending result to master: " + Arrays.toString(recievingBuffer));
+
+      sizeToSend[0] = recievingBuffer.length;
+      int[] numberOfItemsToSend = {1};
+      MPI.COMM_WORLD.Send(numberOfItemsToSend, 0, 1, MPI.INT, 0, 3);
+      MPI.COMM_WORLD.Send(sizeToSend, 0, 1, MPI.INT, 0, 2);
+      MPI.COMM_WORLD.Send(recievingBuffer, 0, sizeToSend[0], MPI.INT, 0, 1);
     }
-
-    private static void puzzleSolverWorker() {
-
   }
 }
---
+++ src/ro/ubb/models/Board.java
@@ -0,0 +1,162 @@
+package ro.ubb.models;
+
+import ro.ubb.Main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board implements Comparable<Board> {
+    private int H;
+    List<Integer> boardTiles;
+    List<Integer> path;
+    int stepsTaken;
+    private int myHash;
+
+    public static Board fromArray(int[] theArray) {
+
+        List<Integer> tiles = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        for(int i = 0; i < Main.SIZE*Main.SIZE;i++)
+        {
+            tiles.add(theArray[i]);
+        }
+        for(int i = Main.SIZE*Main.SIZE+1; i< theArray.length; i++)
+        {
+            path.add(theArray[i]);
+        }
+        return new Board(tiles,path,theArray[Main.SIZE*Main.SIZE]);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Board board = (Board) o;
+        return H == board.H &&
+                stepsTaken == board.stepsTaken &&
+                myHash == board.myHash &&
+                boardTiles.equals(board.boardTiles) &&
+                path.equals(board.path);
+    }
+
+    @Override
+    public String toString() {
+        return "Board{" +
+                "H=" + H +
+                ", boardTiles=" + boardTiles +
+                ", path=" + path +
+                ", stepsTaken=" + stepsTaken +
+                ", myHash=" + myHash +
+                '}';
+    }
+
+    public List<Integer> getBoardTiles() {
+        return boardTiles;
+    }
+
+    public void setBoardTiles(List<Integer> boardTiles) {
+        this.boardTiles = boardTiles;
+    }
+
+    public int getStepsTaken() {
+        return stepsTaken;
+    }
+
+    public void setStepsTaken(int stepsTaken) {
+        this.stepsTaken = stepsTaken;
+    }
+
+    public Board(List<Integer> boardTiles, List<Integer> path, int stepsTaken) {
+        this.boardTiles = boardTiles;
+        this.stepsTaken = stepsTaken;
+        this.path = path;
+        recalculateHash();
+        computeH();
+    }
+
+
+    public int[] flattenBoard()
+    {
+        List<Integer> dummy = new ArrayList<>();
+        dummy.addAll(boardTiles);
+        dummy.add(stepsTaken);
+        dummy.addAll(path);
+        int[] resultArray = new int[boardTiles.size()+path.size()+1];
+        for(int i = 0; i < resultArray.length; i++)
+        {
+            resultArray[i] = dummy.get(i);
+        }
+        return resultArray;
+    }
+
+    public void computeH(){
+
+        int total = 0;
+        for(int i = 0; i < Main.SIZE; i++){
+            for(int j = 0; j < Main.SIZE; j++){
+                if(this.boardTiles.get(i*Main.SIZE+j) != 0) {
+                    int element = boardTiles.get(i*Main.SIZE + j);
+                    int row = (element-1)/ Main.SIZE;
+                    int col = (element-1)% Main.SIZE;
+                    int dist = Math.abs(row-i)+Math.abs(col-j);
+                    total+= dist;
+                }
+                else {
+                    total += Math.abs(i-(Main.SIZE-1)) + Math.abs(j-(Main.SIZE-1));
+                }
+            }
+        }
+        this.H = total;
+    }
+
+    public void recalculateHash() {
+
+        int hashvalue = 0;
+        long modulo = (1L<<56)-5;
+
+        for(int i = 0; i < Main.SIZE*Main.SIZE; i++)
+        {
+                hashvalue = (int) ((hashvalue * Main.SIZE + this.boardTiles.get(i))%modulo);
+        }
+
+        hashvalue = hashvalue ^ (hashvalue >> 10);
+        hashvalue = hashvalue ^ (hashvalue << 5);
+        hashvalue = hashvalue ^ (hashvalue >> 4);
+        hashvalue *= 432534532;
+        hashvalue = hashvalue ^ (hashvalue << 20);
+        hashvalue =hashvalue ^ (hashvalue >> 9);
+        hashvalue = hashvalue ^ (hashvalue <<  5);
+
+
+        myHash = Math.abs(hashvalue);
+    }
+
+    @Override
+    public int hashCode() {
+        return myHash;
+    }
+
+    public int getH() {
+        return H;
+    }
+
+    public int getF(){
+        return  H + stepsTaken;
+    }
+
+    @Override
+    public int compareTo(Board o) {
+        if(this.getF() < o.getF()){
+            return -1;
+        }
+        else if(this.getF()==o.getF())
+        {
+            if(this.stepsTaken < o.stepsTaken)
+                return -1;
+            else if(this.stepsTaken > o.stepsTaken)
+                return 1;
+            return 0;
+        }
+        return 1;
+    }
+}
